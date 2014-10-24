6726a5566a2e340b184c5c2e883b8c48e0dea7b5
lpatzer <laura.patzer@merck.com>
2014-10-24 15:02:15 -0400

    comment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+#######  BEGIN ANSWER
 
-## Write a short comment describing this function
+## Below are a series of functions that can inverse a matrix and cache the findings
 
+## Creating a function that inverses a matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- c()   ## The inverse starts out as empty so it overwrites any previously done calculations
+  set <- function(y) {
+    x <<- y
+    inv <<- c()
+  }
+  get <- function() x    ## Will return the defined matrix
+  setInverse <- function(inverse) inv <<- inverse   ## Finding the inverse of the matrix
+  getInverse <- function() inv
+  list(set = set, get = get,      ## putting all obtained objects into a list
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## Identify if the matrix has changed.  If it has, return a new inverse matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()    ## Obtaining the inverse of the matrix used previously
+  if(!is.null(inv)) {      ## Identifying if the matrix has changed, if not informing user
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)  ## Calculating the new inverse
+  x$setInverse(inv)
+  inv
 }
+
+
+
+
+
+###### TESTING
+
+amatrix = makeCacheMatrix(matrix(c(1,2,3,4), nrow=2, ncol=2))
+amatrix$get()
+cacheSolve(amatrix)
+amatrix$getInverse()
+cacheSolve(amatrix)
+amatrix$set(matrix(c(0,5,99,66), nrow=2, ncol=2)) 
+cacheSolve(amatrix)
+amatrix$get()
+amatrix$getInverse()
+
+
+
+
